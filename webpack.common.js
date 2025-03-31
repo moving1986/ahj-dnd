24efd312b788a3001b020e2513b2843ea3523bc2
Alexey Mezentsev <aleksei_mezencev@mail.ru>
2025-03-31 17:42:33 +0700

    fix

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
 module.exports = {
   target: "web",
   output: {
